107710e91f80d4517c417dac5ab93f5eecfecc6c
ShinPI1 <126882925+ShinPI1@users.noreply.github.com>
2023-03-03 20:07:54 +0700

    Create RequestForm.cpp

---
+++ RequestForm.cpp
@@ -0,0 +1,7 @@
+char* char_gridCountry, * char_country; 
+    char * char_gridDate, * char_date;
+    int int_gridPrice, int_price;
+
+    String^ GridName;
+    String^ GridCountry;
+    String^ GridDate;
