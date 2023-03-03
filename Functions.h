0e5bf6e494a4a775167b30275bc169a7f03f56db
ShinPI1 <126882925+ShinPI1@users.noreply.github.com>
2023-03-03 20:09:01 +0700

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,3 @@
+int add_line_from_adititional_file_to_main_file(char* fileNameFrom, char* fileNameTo);
+void ClearFile(std::string filename);//очистка файла
+bool file_is_empty(std::string fileName);//проверка на пустоту файла
