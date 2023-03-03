00f0bc80298c7e888e007cd028db7b182ddc0a52
ShinPI1 <126882925+ShinPI1@users.noreply.github.com>
2023-03-03 20:10:15 +0700

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,7 @@
+System::Void PharmacyDB::Game::Game_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e)
+{
+	//MessageBox::Show(e->KeyValue.ToString());
+	int left_arrow = 37;
+	int top_arrow = 38;
+	int right_arrow = 39;
+	int bottom_arrow = 40;
