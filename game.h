237f8a6fde5b7c356f8a99778e54e17a4efc3fb5
ShinPI1 <126882925+ShinPI1@users.noreply.github.com>
2023-03-03 20:05:49 +0700

    Create game.h

---
+++ game.h
@@ -0,0 +1,9 @@
+private: System::Void _eatFruit();//поведение при поедании фруктов
+	private: System::Void _MoveSnake();//движение всей структуры змеи
+	private: System::Void _eatItSelf();//поведение при поедании змейкой самой себя
+	private: System::Void _checkBorders();//поведение при встрече с границей
+
+private: System::Void выходToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void вГлавноеМенюToolStripMenuItem_Click_1(System::Object^ sender, System::EventArgs^ e);
+private: System::Void оИгреToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void рекордыToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
