edd8af2fbebd3916571db9329a20c9b54350e4e1
ShinPI1 <126882925+ShinPI1@users.noreply.github.com>
2023-03-03 21:22:39 +0700

    Create AddForm.cpp

---
+++ AddForm.cpp
@@ -0,0 +1,5 @@
+String^ Pharm_numbers1 = maskedTextBoxNumber1->Text->ToString();
+    String^ Pharm_numbers2 = maskedTextBoxNumber2->Text->ToString();
+    String^ Pharm_numbers3 = maskedTextBoxNumber3->Text->ToString();
+    String^ Pharm_numbers4 = maskedTextBoxNumber4->Text->ToString();
+    String^ Pharm_numbers5 = maskedTextBoxNumber5->Text->ToString();
