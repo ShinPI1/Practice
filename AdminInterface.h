b66e481badd0916ad4c4e6d099dabd9c9940f465
ShinPI1 <126882925+ShinPI1@users.noreply.github.com>
2023-03-04 01:45:08 +0700

    Create AdminInterface.h

---
+++ AdminInterface.h
@@ -0,0 +1,17 @@
+		void InitializeComponent(void)
+		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->выходToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->выйтиИзПрограммыToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->вернутьсяНазадToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->dataGridViewAdmin = (gcnew System::Windows::Forms::DataGridView());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->buttonDeleteLine = (gcnew System::Windows::Forms::Button());
+			this->buttonEditLine = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->buttonAddLine = (gcnew System::Windows::Forms::Button());
+			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
+			this->buttonDoRequest = (gcnew System::Windows::Forms::Button());
+			this->menuStrip1->SuspendLayout();
