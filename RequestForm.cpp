ed37ae14a6e54cfb3233a02bd2e563719a4b34d4
ShinPI1 <126882925+ShinPI1@users.noreply.github.com>
2023-03-03 21:25:19 +0700

    Update RequestForm.cpp

--- RequestForm.cpp
+++ RequestForm.cpp
@@ -16,3 +16,21 @@ row_is_true = true;
         GridNumber5 = Convert::ToString(dataGridViewRequest->Rows[row]->Cells[8]->Value);
         GridPrice = Convert::ToString(dataGridViewRequest->Rows[row]->Cells[9]->Value);
         Convert_String_to_string(GridName, gridName);
+        char_gridCountry = Convert_String_to_char(GridCountry);
+        char_country = Convert_String_to_char(Country);
+        Convert_String_to_string(GridDate, gridDate);
+        char_gridDate = Convert_String_to_char(GridDate);
+        char_date = Convert_String_to_char(Date);
+        Convert_String_to_string(GridNumber1, gridNumber1);
+        Convert_String_to_string(GridNumber2, gridNumber2);
+        Convert_String_to_string(GridNumber3, gridNumber3);
+        Convert_String_to_string(GridNumber4, gridNumber4);
+        Convert_String_to_string(GridNumber5, gridNumber5);
+        Convert_String_to_string(GridPrice, gridPrice);
+       
+        if (date != "") {
+            if (strcmp(char_gridDate, char_date) <= 0) row_is_true = false;
+        }
+        if (number != "") {
+            if (number != gridNumber1 && number != gridNumber2 && number != gridNumber3 && number != gridNumber4 && number != gridNumber5) row_is_true = false;
+        }
